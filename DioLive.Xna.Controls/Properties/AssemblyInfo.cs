7dbbf428b82c5191fa6467be9ad995b128e4e5b6
DioLive Studio <dio@live.ru>
2016-06-05 13:11:17 +0300

    Fill assembly info

--- DioLive.Xna.Controls/Properties/AssemblyInfo.cs
+++ DioLive.Xna.Controls/Properties/AssemblyInfo.cs
@@ -8,9 +8,9 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("DioLive.Xna.Controls")]
 [assembly: AssemblyProduct("DioLive.Xna.Controls")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyDescription("UI Controls demo application for XNA/Monogame")]
+[assembly: AssemblyCompany("DioLive Studio")]
+[assembly: AssemblyCopyright("© 2016 DioLive Studio")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.1.*")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
