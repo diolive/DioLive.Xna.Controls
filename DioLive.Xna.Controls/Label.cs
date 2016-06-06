343a7fd36c94b9530c30807e6e661c5c05b0acfe
DioLive Studio <dio@live.ru>
2016-06-06 11:07:14 +0300

    Add Label preview

---
+++ DioLive.Xna.Controls/Label.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework;
+
+namespace DioLive.Xna.Controls
+{
+    public class Label : UIElement
+    {
+        public Label()
+        {
+            Text = string.Empty;
+        }
+
+        public string Text { get; set; }
+
+        public override void Draw(SpriteBatch spriteBatch)
+        {
+            base.Draw(spriteBatch);
+
+            //var gfx = spriteBatch.GraphicsDevice;
+
+            //using (Scope.UseValue(() => gfx.RasterizerState, Assets.Scissors))
+            {
+                //using (Scope.UseValue(() => gfx.ScissorRectangle, Rectangle.Intersect(this.GetInnerBounds(), gfx.ScissorRectangle)))
+                {
+                    spriteBatch.DrawString(Assets.PTSans14, this.Text, new Vector2(30, 30), Color.Black);
+                }
+            }
+        }
+    }
+}
