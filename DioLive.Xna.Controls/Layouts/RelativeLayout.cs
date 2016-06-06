0516dc50324e9be961923e6b0357253e1ad14752
DioLive Studio <dio@live.ru>
2016-06-06 11:05:04 +0300

    Add RelativeLayout

---
+++ DioLive.Xna.Controls/Layouts/RelativeLayout.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+using Microsoft.Xna.Framework;
+
+namespace DioLive.Xna.Controls.Layouts
+{
+    public class RelativeLayout : ILayout
+    {
+        private Container container;
+
+        public RelativeLayout(Container container)
+        {
+            this.container = container;
+        }
+
+        public Rectangle GetBounds(UIElement element)
+        {
+            if (!this.container.Elements.Contains(element))
+            {
+                throw new ArgumentException("Element was not found in this layout");
+            }
+
+            return new Rectangle((this.container.Location + element.Location).ToPoint(), element.Size.ToPoint());
+        }
+
+        void ILayout.Invalidate()
+        {
+        }
+
+        public static LayoutBuilder GetBuilder() => (c) => new RelativeLayout(c);
+    }
+}
