1ef4550019917705494ce85fbb07b1f834636ed1
DioLive Studio <dio@live.ru>
2016-06-06 22:18:43 +0300

    Add PT Sans font with different sizes

--- DioLive.Xna.Controls/Assets.cs
+++ DioLive.Xna.Controls/Assets.cs
@@ -7,14 +7,38 @@ namespace DioLive.Xna.Controls
     {
         public static Texture2D Pixel { get; private set; }
 
+        public static SpriteFont PTSans8 { get; private set; }
+
+        public static SpriteFont PTSans10 { get; private set; }
+
+        public static SpriteFont PTSans12 { get; private set; }
+
         public static SpriteFont PTSans14 { get; private set; }
+
+        public static SpriteFont PTSans16 { get; private set; }
+
+        public static SpriteFont PTSans18 { get; private set; }
+
+        public static SpriteFont PTSans24 { get; private set; }
+
+        public static SpriteFont PTSans36 { get; private set; }
+
+        public static SpriteFont PTSans48 { get; private set; }
 
         public static RasterizerState Scissors { get; private set; }
 
         public static void Load(ContentManager content)
         {
             Pixel = content.Load<Texture2D>("textures/pixel");
-            PTSans14 = content.Load<SpriteFont>("fonts/PT Sans, 14");
+            PTSans8 = content.Load<SpriteFont>("fonts/PTSans8");
+            PTSans10 = content.Load<SpriteFont>("fonts/PTSans10");
+            PTSans12 = content.Load<SpriteFont>("fonts/PTSans12");
+            PTSans14 = content.Load<SpriteFont>("fonts/PTSans14");
+            PTSans16 = content.Load<SpriteFont>("fonts/PTSans16");
+            PTSans18 = content.Load<SpriteFont>("fonts/PTSans18");
+            PTSans24 = content.Load<SpriteFont>("fonts/PTSans24");
+            PTSans36 = content.Load<SpriteFont>("fonts/PTSans36");
+            PTSans48 = content.Load<SpriteFont>("fonts/PTSans48");
             Scissors = new RasterizerState { ScissorTestEnable = true };
         }
 
