d0489b21161a7c614f47314423e5be8e5dcf5a88
DioLive Studio <dio@live.ru>
2016-06-06 11:04:22 +0300

    Reorganize assets, add font

--- DioLive.Xna.Controls/Assets.cs
+++ DioLive.Xna.Controls/Assets.cs
@@ -7,11 +7,14 @@ namespace DioLive.Xna.Controls
     {
         public static Texture2D Pixel { get; private set; }
 
+        public static SpriteFont PTSans14 { get; private set; }
+
         public static RasterizerState Scissors { get; private set; }
 
         public static void Load(ContentManager content)
         {
-            Pixel = content.Load<Texture2D>("pixel");
+            Pixel = content.Load<Texture2D>("textures/pixel");
+            PTSans14 = content.Load<SpriteFont>("fonts/PT Sans, 14");
             Scissors = new RasterizerState { ScissorTestEnable = true };
         }
 
